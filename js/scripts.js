1062a31c01124218d51d3749a7757b39dccdc75d
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-11-08 13:39:49 +0100

    Update js/scripts.js
    // 3: Select all elements with the class '.card'.
    //    Set their background color to the color of your choice.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,19 @@
-objectToArray = (object) => {
-    return [].slice.call(object);
-}
+objectToArray = (object) => {return [].slice.call(object);}
 
 // 1: Select the element with the ID 'about'. 
 //    Store the element in the variable `about`.
 const about = document.getElementById('about');
-about.style.border = "2px solid firebrick";
+about.style.border = '2px solid firebrick';
 
 // 2: Select all the <h2> elements in the document.
 //    Set the color of the <h2> elements to a different color.
 const h2 = objectToArray(document.getElementsByTagName('h2'));
-h2.forEach(heading => {
-    heading.style.color = "firebrick";
-});
+h2.forEach(heading => {heading.style.color = "firebrick";});
 
 // 3: Select all elements with the class '.card'. 
 //    Set their background color to the color of your choice.
-
+const card = objectToArray(document.querySelectorAll('.card'));
+card.forEach(card => {card.style.backgroundColor = 'lightGray';});
 
 // 4: Select only the first <ul> in the document.
 //    Assign it to a variable named `ul`.
